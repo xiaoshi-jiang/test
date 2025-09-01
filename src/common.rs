699e83f1d063fbcb1d6572e049ce34c5b800eeca
xiaoshi-jiang <82742793+xiaoshi-jiang@users.noreply.github.com>
2025-09-01 22:29:47 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://192.168.199.222:21114".to_owned()
+    "http://38.207.191.70:21114".to_owned()
 }
 
 #[inline]
